17086a2b07a890c003035798dd25d7dd02f91792
Jero1015 <jeromedelacruz1015@gmail.com>
2024-05-25 15:09:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const btnLike1 = document.getElementById("btnLike1")
 const countLikes1 = document.getElementById("countLikes1")
+const btnLike2 = document.getElementById("btnLike2")
+const countLikes2 = document.getElementById("countLikes2")
 
 function clickLike1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
