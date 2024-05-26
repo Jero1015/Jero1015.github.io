8a56a3fe1b4de4ccfff164efb8331fb65805bc18
Jero1015 <jeromedelacruz1015@gmail.com>
2024-05-26 21:13:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const countLikes1 = document.getElementById("countLikes1")
 const btnLike2 = document.getElementById("btnLike2")
 const countLikes2 = document.getElementById("countLikes2")
 
-const btnDisLike1 = document.getElementById("btnDisLike1")
-const countDisLikes1 = document.getElementById("countDisLikes1")
-const btnDisLike2 = document.getElementById("btnDisLike2")
-const countDisLikes2 = document.getElementById("countDisLikes2")
+const btnHaha1 = document.getElementById("btnHaha1")
+const countHaha1 = document.getElementById("countHaha1")
+const btnHaha2 = document.getElementById("btnHaha2")
+const countHaha2 = document.getElementById("countHaha2")
+
+
+const btnDislikes1 = document.getElementById("btnDislike1")
+const countDislikes1 = document.getElementById("countDislikes1")
+const btnDislikes2 = document.getElementById("btnDislike2")
+const countDislikes2 = document.getElementById("countDislikes2")
+
 function clickLike1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
   countLikes1.textContent = totalLikes1.toString()
@@ -16,16 +23,29 @@ function clickLike2() {
   countLikes2.textContent = totalLikes2.toString()
 }
 
-function clickDisLike1() {
-  let totalDisLikes1 = parseInt(countDisLikes1.value) + 1
-  countDisLikes1.textContent = totalDisLikes1.toString()
+function clickHaha1(){
+  let totalHaha1 = parseInt(countHaha1.value) + 1
+  countHaha1.textContent = totalHaha1.toString()
 }
-function clickDisLike2() {
-  let totalDisLikes2 = parseInt(countDisLikes2.value) + 1
-  countDisLikes2.textContent = totalDisLikes2.toString()
+function clickHaha2(){
+  let totalHaha2 = parseInt(countHaha2.value) + 1
+  countHaha2.textContent = totalHaha2.toString()
 }
+
+
+function clickDislike1(){
+  let totalDislikes1 = parseInt(countDislikes1.value) + 1
+  countDislikes1.textContent = totalDislikes1.toString()
+}
+function clickDislike2(){
+  let totalDislikes2 = parseInt(countDislikes2.value) + 1
+  countDislikes2.textContent = totalDislikes2.toString()
+}
+
+
 btnLike1.addEventListener("click",clickLike1)
 btnLike2.addEventListener("click",clickLike2)
-
-btnDisLike1.addEventListener("click",clickDisLike1)
-btnDisLike2.addEventListener("click",clickDisLike2)
+btnHaha1.addEventListener("click",clickHaha1)
+btnHaha2.addEventListener("click",clickHaha2)
+btnDislike1.addEventListener("click",clickDislike1)
+btnDislike2.addEventListener("click",clickDislike2)
