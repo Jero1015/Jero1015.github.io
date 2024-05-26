5e3fcdd07907455f610fa42254b791cd358aeb9c
Jero1015 <jeromedelacruz1015@gmail.com>
2024-05-26 16:00:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const countLikes1 = document.getElementById("countLikes1")
 const btnLike2 = document.getElementById("btnLike2")
 const countLikes2 = document.getElementById("countLikes2")
 
-const btnHAHA🤣1 = document.getElementById("btnHAHA🤣1")
+const btnHAHA1 = document.getElementById("btnHAHA1")
 const countHAHA1 = document.getElementById("countHAHA1")
-const btnHAHA🤣2 = document.getElementById("btnHAHA🤣2")
+const btnHAHA2 = document.getElementById("btnHAHA2")
 const countHAHA2 = document.getElementById("countHAHA2")
 function clickLike1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
@@ -16,16 +16,16 @@ function clickLike2() {
   countLikes2.textContent = totalLikes2.toString()
 }
 
-function clickHAHA🤣1() {
+function clickHAHA1() {
   let totalHAHA1 = parseInt(counHAHA1.value) + 1
   countHAHA1.textContent = totalHAHA1.toString()
 }
-function clickHAHA🤣2() {
+function clickHAHA2() {
   let totalHAHA2 = parseInt(countHAHA2.value) + 1
   countHAHA2.textContent = totalHAHA2.toString()
 }
 btnLike1.addEventListener("click",clickLike1)
 btnLike2.addEventListener("click",clickLike2)
 
-btnHAHA1.addEventListener("click",clickHAHA🤣1)
-btnHAHA2.addEventListener("click",clickHAHA🤣2)
+btnHAHA1.addEventListener("click",clickHAHA1)
+btnHAHA2.addEventListener("click",clickHAHA2)
